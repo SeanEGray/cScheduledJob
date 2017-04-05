e0f731af2ef399a752af0c9bf1db3dea7d619827
Sean Gray <sean.e.gray@outlook.com>
2017-04-05 22:34:14 +0100

    Add basic psscriptanalyzer tests

---
+++ Tests/PSScriptAnalyzer.Tests.ps1
@@ -0,0 +1,16 @@
+<# 
+	Run everything through PSScriptAnalyzer
+#>
+
+$Rules   = Get-ScriptAnalyzerRule
+$PSFiles = Get-ChildItem '..\DSCResources\' -Filter '*.psm1' -Recurse
+
+foreach ($PSFile in $PSFiles) {
+	Describe $PSFile {
+		foreach ($Rule in $Rules) {
+			It "passes the PSScriptAnalyzer Rule $Rule" {
+				(Invoke-ScriptAnalyzer -Path $PSFile.FullName -IncludeRule $rule.RuleName).Count | Should Be 0
+            }
+        }
+    }
+}
