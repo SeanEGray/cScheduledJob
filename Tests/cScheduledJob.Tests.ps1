d44852883e87f929d324185eb1074152bd4edcc8
SeanEGray <sean-github@tacit.ly>
2017-04-21 11:20:11 +0100

    Refactor tests to stop scriptanalyzer complaining

--- Tests/cScheduledJob.Tests.ps1
+++ Tests/cScheduledJob.Tests.ps1
@@ -1,3 +1,24 @@
+<# 
+	Run everything through PSScriptAnalyzer
+#>
+
+$Rules   = Get-ScriptAnalyzerRule
+$PSFiles = Get-ChildItem "$PSScriptRoot\..\" -Filter '*.psm1' -Recurse
+
+foreach ($PSFile in $PSFiles) {
+	Describe "ScriptAnalyzer: $PSFile" {
+		foreach ($Rule in $Rules) {
+			It "passes the PSScriptAnalyzer Rule $Rule" {
+				(Invoke-ScriptAnalyzer -Path $PSFile.FullName -IncludeRule $rule.RuleName).Count | Should Be 0
+            }
+        }
+    }
+}
+
+<#
+	Functional tests
+#>
+
 Using module ..\cScheduledJob.psm1
 
 Describe 'Get' {
